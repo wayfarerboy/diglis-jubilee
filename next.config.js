d7b1379e654fb7d88ed4aee546b1c4fc95c78408
Al Pagan <generamics@gmail.com>
2022-04-14 22:44:58 +0100

    Switched off caching by default until we can make it optional

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   reactStrictMode: true,
   pwa: {
     dest: 'public',
-    disable: process.env.NODE_ENV === 'development',
+    disable: true || process.env.NODE_ENV === 'development',
   },
   images: {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384, 512],
